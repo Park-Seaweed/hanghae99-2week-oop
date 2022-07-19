9adf489ebc40c1c5e70378dbe637935ec8def3e6
Park-Seaweed <qkralsgur721@gmail.com>
2022-07-20 02:45:15 +0900

    상위 클래스 생성중

--- hanghae99-2week-oop/src/main/java/PublicTraffic.java
+++ hanghae99-2week-oop/src/main/java/PublicTraffic.java
@@ -1,2 +1,20 @@
 public class PublicTraffic {
+    int num;
+    int speedChange;
+    int peopleMax;
+    int fuelVolume;
+    int speed;
+
+    public PublicTraffic(int num, int speedChange, int peopleMax, int fuelVolume, int speed) {
+        this.num = num;
+        this.speedChange = speedChange;
+        this.peopleMax = peopleMax;
+        this.fuelVolume = fuelVolume;
+        this.speed = speed;
+    }
+
+    public PublicTraffic(int num, int speedChange, int peopleMax) {
+        this(num, speedChange, peopleMax, 100,0);
+    }
+    
 }
